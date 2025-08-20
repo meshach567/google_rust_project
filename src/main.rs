d20f79f07be2ae197217b84a02219134d49289cf
Meshach Arinze <arinzemeshach123@gmail.com>
2025-08-20 17:52:21 +0100

    fix: solve the google fibonacci exercise with rust

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-fn interproduct(a: i16, b: i16, c: i16) -> i16 {
-    return (a * b).saturating_add(b * c).saturating_add(c * a);
+fn fib(n: u32) -> u32 {
+    if n < 2 {
+        return n
+    } else {
+        return fib(n - 1) + fib(n - 2)
+    }
 }
 
 fn main() {
-    println!("result: {}", interproduct(120, 100, 248));
+    let n = 10;
+    println!("The {}th Fibonacci number is: {}", n, fib(n))
 }
