13a4aed229bbbe67d01f004bbe883b41085bc983
Meshach Arinze <arinzemeshach123@gmail.com>
2025-08-29 15:08:03 +0100

    refactor: add a params obj Uri, Method, and Body

--- active_web/api-tests.ps1
+++ active_web/api-tests.ps1
@@ -3,3 +3,14 @@ Invoke-RestMethod -Uri http://localhost:6570/vehicle -Method Get
 
 # post new vehicle
 Invoke-RestMethod -Uri http://localhost:6570/vehicle -Method Post
+
+$Params = @{
+    Uri = 'https://localhost:6570/vehicle'
+    Method = 'Post'
+    Body = @{
+        manufacturer = 'Tesla'
+        model = 'Model Y'
+        year = 2024
+    } | ConvertTo-Json
+}
+Invoke-RestMethod @Params
