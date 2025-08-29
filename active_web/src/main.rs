e1feb46c74977b1b02e5d7110c3eb3f3f795e13f
Meshach Arinze <arinzemeshach123@gmail.com>
2025-08-29 12:04:14 +0100

    fix: import the exrernal module vehicle

---
+++ active_web/src/main.rs
@@ -0,0 +1,21 @@
+use axum::{debug_handler, routing::{get, post}, Json, Router};
+
+mod vehicle;
+
+use vehicle::{vehicle_get, vehicle_post};
+
+#[tokio::main]
+
+pub async fn main() {
+    // Create the axum router
+    let router01 = Router::new().route("/vehicle", get(vehicle::vehicle_get))
+    .route("/vehicle", post(vehicle::vehicle_post));
+
+    //2. Define the IP and port to listen on
+    let address = "0.0.0.0:6570";
+    let listener = tokio::net::TcpListener::bind(address).await.unwrap();
+
+    // 3. axum serve to launch the web server
+    axum::serve(listener, router01).await.unwrap();
+}
+
