bf87eae74501290c73748b89f32db15a55c71497
Meshach Arinze <arinzemeshach123@gmail.com>
2025-08-29 12:03:50 +0100

    test: invole the endpoint with powershell

---
+++ active_web/api-tests.ps1
@@ -0,0 +1,5 @@
+# Get vehicle basic example
+Invoke-RestMethod -Uri http://localhost:6570/vehicle -Method Get
+
+# post new vehicle
+Invoke-RestMethod -Uri http://localhost:6570/vehicle -Method Post
