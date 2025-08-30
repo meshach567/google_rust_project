e3f7be027ef7035e82a936aef25b2c66609a8b83
Meshach Arinze <arinzemeshach123@gmail.com>
2025-08-30 20:59:52 +0100

    feat: add route parameter for query

--- active_web/api-tests.ps1
+++ active_web/api-tests.ps1
@@ -12,5 +12,20 @@ $Params = @{
         model = 'Model Y'
         year = 2024
     } | ConvertTo-Json
+    ContentType = 'application/json'
+}
+Invoke-RestMethod @Params
+
+$Params = @{
+    Uri = 'http://localhost:6570/vehicle?manufacturer=Tesla&model=Model Y&year=2024'
+    Method = 'Post'
+}
+Invoke-RestMethod @Params
+
+# Uses query string parameters instead of body payload
+
+$Params = @{
+    Uri = 'http://localhost:6570/vehicle?manufacturer=Tesla&model=Model Y&year=2024'
+    Method = 'Post'
 }
 Invoke-RestMethod @Params
